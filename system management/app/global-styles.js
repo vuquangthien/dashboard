b8198f0db8223ec9ca79b83ab54fed656f5d1110
unknown <vuquangthien191223@gmail.com>
2021-06-29 08:36:14 +0700

    Thêm mới dự án

---
+++ system management/app/global-styles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  #app {
+    background-color: #fafafa;
+    min-height: 100%;
+    min-width: 100%;
+  }
+
+  p,
+  label {
+    line-height: 1.5em;
+  }
+`;
+
+export default GlobalStyle;
